7b5a750918b58cc77dded4d7e3696aea729961ad
Robert Spitzenpfeil <robert@linuxbox.site>
2011-10-05 11:15:48 +0200

    'make_all-pngs.sh' now uses the drill file as well.

--- gerber/make_all-pngs.sh
+++ gerber/make_all-pngs.sh
@@ -4,4 +4,8 @@ for file in `ls *.g[!v]?`; do
 gerbv $file -o $file.png --dpi=300x300 -a --export=png
 done
 
+for file in `ls *.drl`; do
+gerbv $file -o $file.png --dpi=300x300 -a --export=png
+done
+
 mv *.png ./PNGs
